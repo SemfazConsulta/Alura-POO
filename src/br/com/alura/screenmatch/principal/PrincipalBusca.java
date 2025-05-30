b0d147dfdc08025d3f1c3499899ec3e291767531
CARLOS25052003 <carloscharles016@outlook.com>
2025-05-30 00:46:56 -0400

    Pacote java.io com FileWriter

--- src/br/com/alura/screenmatch/principal/PrincipalBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalBusca.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -43,6 +44,11 @@ public class PrincipalBusca {
             Titulo meuTitulo =  new Titulo(meuTituloOmd);
             System.out.println("Titulo ja convertido");
             System.out.println(meuTitulo);
+
+            FileWriter escrita = new FileWriter("filmes.txt");
+            escrita.write(meuTitulo.toString());
+            escrita.close();
+
         } catch (NumberFormatException e){
             System.out.print("Aconteceu um erro: ");
             System.out.println(e.getMessage());
